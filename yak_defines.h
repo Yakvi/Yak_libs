94b5bacd61a1da2abb25dbea300945284ea894ff
Ivan <ivankoy@gmail.com>
2019-08-21 08:40:57 +0200

    First commit

---
+++ yak_defines.h
@@ -0,0 +1,162 @@
+#if !defined(YAK_DEFINES)
+// clang-format off
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+//
+// NOTE: Assertions
+//
+#if DEBUG
+#define Assert(Expression) if (!(Expression)) { *(int *)0 = 0; } // Point to sector 0, immediately crash
+#else
+#define Assert(Expression)
+#endif
+
+#define InvalidCodePath Assert(!"InvalidCodePath")
+#define InvalidDefaultCase default: { InvalidCodePath; } break
+
+//
+// NOTE: Compilers
+//
+#if !defined(COMPILER_MSVC)
+#define COMPILER_MSVC 0
+#endif // !COMPILER_MSVC
+
+#if !defined(COMPILER_LLVM)
+#define COMPILER_LLVM 0
+#endif // !COMPILER_LLVM
+
+#if !COMPILER_MSVC && !COMPILER_LLVM
+#if _MSC_VER
+#undef COMPILER_MSVC
+#define COMPILER_MSVC 1
+#else
+// TODO: MORE compilers!
+#undef COMPILER_LLVM
+#define COMPILER_LLVM 1
+#endif
+#endif
+
+#if COMPILER_MSVC
+#include <intrin.h>
+#elif COMPILER_LLVM
+#include <x86intrin.h>
+#else
+#error SEE/NEON optimizations are not available for this compiler yet!!!!
+#endif
+
+//
+// NOTE: Types
+//
+#if !defined(internal)
+#define internal static
+#endif
+#define local_persist static
+#define global_variable static
+
+// #include <stddef.h>
+// #include <limits.h>
+// #include <float.h>
+#include <stdint.h>
+
+typedef int8_t s8;
+typedef int16_t s16;
+typedef int32_t s32;
+typedef int64_t s64;
+typedef s32 b32;
+typedef uint8_t u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+typedef uintptr_t uintptr;
+typedef size_t memory_index;
+typedef float f32;
+typedef double f64;
+
+#define Kilobytes(Value) ((Value)*1024LL)
+#define Megabytes(Value) (Kilobytes(Value) * 1024LL)
+#define Gigabytes(Value) (Megabytes(Value) * 1024LL)
+#define Terabytes(Value) (Gigabytes(Value) * 1024LL)
+
+//
+// NOTE: String utlities
+// 
+typedef char const* str; // TODO: what happens to it in memory? Possible leaks?
+inline b32 FillBuffer(char* Source, char *Dest, u32 MaxLength)
+{
+    b32 Result = true;
+    u32 BufferIndex = 0;
+    char *At = Source;
+    while(At)
+    {
+        Dest[BufferIndex++] = *At++;
+        if(BufferIndex >= MaxLength)
+        {
+            Result = false;
+            break;
+        }
+    }
+    
+    return(Result);
+}
+
+// 
+// NOTE: Array Utilities
+// 
+#define ArrayCount(Array) (sizeof(Array) / sizeof((Array)[0]))
+#define InArray(Count, Array) (Count < ArrayCount(Array))
+// TODO: swap, min, max ... macros?
+
+//
+// NOTE: Memory Utilities
+// 
+#define AlignPow2(Value, Alignment) \
+    ((Value + ((Alignment)-1)) & ~((Alignment)-1))
+#define Align4(Value) ((Value + 3) & ~3)
+#define Align8(Value) ((Value + 7) & ~7)
+#define Align16(Value) ((Value + 15) & ~15)
+
+#define Pi32 3.14159265359f
+#define Tau32 6.28318530717958647692f
+#define Real32Maximum FLT_MAX
+#define UIntMin 0
+#define UInt32Max 0xFFFFFFFF
+#define UInt16Max 65535
+#define Int16Min -32768
+#define Int16Max 32767
+
+inline u32
+SafeTruncateUInt64(u64 Value)
+{
+    Assert(Value <= UInt32Max);
+    u32 Result = (u32)Value;
+    return (Result);
+}
+
+inline u16
+SafeTruncateToUInt16(u32 Value)
+{
+    Assert(Value <= UInt16Max);
+    Assert(Value >= UIntMin);
+    u16 Result = (u16)Value;
+    return (Result);
+}
+
+inline s16
+SafeTruncateToInt16(s32 Value)
+{
+    Assert(Value <= Int16Max);
+    Assert(Value >= Int16Min);
+    s16 Result = (s16)Value;
+    return (Result);
+}
+
+#ifdef __cplusplus
+}
+#endif
+
+// clang-format on
+#define YAK_DEFINES 1
+#endif
---
+++ yak_test.h
@@ -0,0 +1,186 @@
+#if !defined(YAK_TEST)
+
+///
+/// API
+///
+// YTestShowResults();              // Call after you define your tests.
+// YTestCleanup();                  // Call if you wish to reset your tests with new data.
+// Y_TEST_CATEGORY(Description);    Use this to split your tests into groups. Not nestable.
+// Y_ASSERT(Name, Test);            Define your tests. These are executed right away, the results are stored for later use.
+//
+//
+
+#include <malloc.h>
+#include <stdio.h>
+// TODO: Platform specific allocs
+#define ALLOC(item) (item*)malloc(sizeof(item))
+#define FREELISTITEM(item, temp) \
+    if (item)                    \
+    {                            \
+        temp = item->Next;       \
+        free(item);              \
+        item = temp;             \
+    }
+
+struct condition;
+struct test;
+
+inline void GetNextTestSlot(const char* Name);
+inline void PopulateConditionAndAdvance(const char* Name, const char* TestText, size_t TestValue);
+static condition* InitializeTestGlobals();
+
+static test* GlobalFirstTestSlot;
+static test* GlobalCurrentTestSlot;
+static condition* GlobalCurrentCondition = InitializeTestGlobals();
+
+#define Y_TEST_CATEGORY(Name) GetNextTestSlot(Name);
+#define Y_ASSERT(Name, Test) PopulateConditionAndAdvance(Name, #Test, (size_t)Test);
+
+//
+// BOOKMARK: IMPLEMENTATION
+//
+
+struct condition
+{
+    const char* Description;
+    const char* TestDescription;
+    unsigned Result;
+
+    condition* Next;
+};
+
+struct test
+{
+    const char* Description;
+    condition* FirstCondition;
+    unsigned ConditionCount;
+
+    test* Next;
+};
+
+inline condition*
+GetCondition()
+{
+    condition* Result = ALLOC(condition);
+
+    Result->Next = 0;
+    Result->Description = 0;
+    Result->TestDescription = 0;
+    Result->Result = 0;
+
+    return (Result);
+}
+
+inline test*
+GetTestSlot(const char* Name)
+{
+    test* Result = ALLOC(test);
+
+    Result->Description = Name;
+    Result->ConditionCount = 0;
+    Result->FirstCondition = 0;
+    Result->Next = 0;
+
+    return (Result);
+}
+
+static condition*
+InitializeTestGlobals()
+{
+    GlobalFirstTestSlot = GlobalCurrentTestSlot = GetTestSlot(0);
+    condition *FirstCondition = GlobalFirstTestSlot->FirstCondition = GetCondition();
+
+    return (FirstCondition);
+}
+
+inline void
+GetNextTestSlot(const char* Name)
+{
+
+    test *NextTest = GetTestSlot(Name);
+    
+    GlobalCurrentTestSlot->Next = NextTest;
+    GlobalCurrentTestSlot = GlobalCurrentTestSlot->Next;
+    
+    GlobalCurrentCondition = NextTest->FirstCondition = GetCondition();
+}
+
+inline void
+PopulateConditionAndAdvance(const char* Name, const char* TestText, size_t TestValue)
+{
+    GlobalCurrentTestSlot->ConditionCount++;
+    
+    GlobalCurrentCondition->Description = Name;
+    GlobalCurrentCondition->TestDescription = TestText;
+    GlobalCurrentCondition->Result = (unsigned)TestValue;
+    GlobalCurrentCondition->Next = GetCondition();
+
+    GlobalCurrentCondition = GlobalCurrentCondition->Next;
+}
+
+static void
+YTestShowResults(void)
+{
+    for (test* Test = GlobalFirstTestSlot;
+         Test;
+         Test = Test->Next)
+    {
+        if (Test->ConditionCount > 0)
+        {
+            printf("* %s", Test->Description ? Test->Description : "Base asserts");
+            printf("\n");
+
+            for (condition* Condition = Test->FirstCondition;
+                 Condition->Next; // we stop if we don't see a next condition, this one is empty
+                 Condition = Condition->Next)
+            {
+                // TODO: Colored text
+                printf("\t- %-30s: %s (test input: '%s')\n",
+                       Condition->Description,
+                       Condition->Result ? "PASSED" : "FAILED",
+                       Condition->TestDescription);
+            }
+        }
+    }
+}
+
+/** Free memory of each condition and each test */
+static void
+YTestCleanup(void)
+{
+    // Free tests linked list
+    test* CurrentSlot = GlobalFirstTestSlot->Next;
+    test* NextSlot;
+    for (;;)
+    {
+        if (CurrentSlot)
+        {
+            // Free conditions linked list for each test
+            condition* CurrentCondition = CurrentSlot->FirstCondition;
+            condition* NextCondition;
+            for (;;)
+            {
+                if (CurrentCondition)
+                {
+                    FREELISTITEM(CurrentCondition, NextCondition);
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            FREELISTITEM(CurrentSlot, NextSlot);
+        }
+        else
+        {
+            break;
+        }
+    }
+
+    // Reinitialize the globals
+    GlobalCurrentCondition = InitializeTestGlobals();
+}
+
+#define YAK_TEST 1
+#endif // !(YAK_TEST)
